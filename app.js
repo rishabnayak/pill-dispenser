a3a7f7f905a126c4dedcd97b26d44078471e4320
rishab2113 <rishab_nayak@yahoo.com>
2018-05-31 21:25:16 -0400

    basic webpage with bootstrap, socket.io, and express

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.use(express.static(__dirname + '/views'));
+app.engine('html', require('ejs').renderFile);
+app.get('/', (req, res) => {
+  res.render('homepage.html');
+});
+io.on('connection',function(socket){
+  console.log('user connected');
+    socket.on('disconnect',() => {
+      console.log('user disconnected');
+    });
+});
+http.listen(8080, () => {
+console.log("View at localhost:8080");
+});
