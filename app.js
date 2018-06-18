80b4e283e57375d830563fcd28612f67f46a1f7e
ek210 <blah>
2018-06-17 21:30:46 -0400

    nxt

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const i2cbus = require('i2c-bus');
+const sleep = require('sleep');
+const bus = i2cbus.openSync(1);
+const addr = 0x40;
 app.use(express.static(__dirname + '/views'));
 app.engine('html', require('ejs').renderFile);
 app.get('/', (req, res) => {
@@ -14,8 +18,15 @@ io.on('connection',function(socket){
     var motorvalue = 0;
     socket.on('motor', function(data){
        lightvalue = data;
-       if (lightvalue) {
-           console.log(lightvalue)
+       if (lightvalue = 1) {
+           bus.writeByteSync(addr, 0, 0x20);
+           bus.writeByteSync(addr, 0xfe, 0x1e);
+           bus.writeWordSync(addr, 0x06, 0);
+           bus.writeWordSync(addr, 0x08, 1250);
+           sleep.sleep(1);
+           bus.writeWordSync(addr, 0x08, 836);
+           sleep.sleep(1);
+           bus.writeWordSync(addr, 0x08, 1664);
        }
     });
 });
