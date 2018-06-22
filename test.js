b950d8da9f242e889b2751e912a8e43205094a5b
rishab2113 <rishab_nayak@yahoo.com>
2018-06-22 19:54:02 -0400

    x

--- test.js
+++ test.js
@@ -5,6 +5,5 @@ const addr = 0x40;
 bus.writeByteSync(addr, 0, 0x20);
 bus.writeByteSync(addr, 0xfe, 0x1e);
 bus.writeWordSync(addr, 0x06, 0);
-bus.writeWordSync(addr, 0x08, 1250)
 sleep.sleep(1);
 bus.writeWordSync(addr, 0x08, 836);
