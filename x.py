91351cd6d13bcb1cab890a163eee7f2470af3c74
rishab2113 <rishab_nayak@yahoo.com>
2018-06-22 21:05:06 -0400

    s

--- x.py
+++ x.py
@@ -9,7 +9,8 @@ addr = 0x40           # I2C address of the PWM chip.
 bus.write_byte_data(addr, 0, 0x20)     # enable the chip
 bus.write_byte_data(addr, 0xfe, 0x1e)  # configure the chip for multi-byte write
 
-bus.write_word_data(addr, 0x06, 0)     # chl 0 start time = 0us  # chl 0 end time = 1.5ms
+bus.write_word_data(addr, 0x06, 0)     # chl 0 start time = 0us
+bus.write_word_data(addr, 0x08, 1250)  # chl 0 end time = 1.5ms
 
 ## The 1.5ms end time is equal to 1.2us per count. This represents the neutral
 ##  position of the servo, midway between both extremes. Each degree of
