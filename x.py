ec25e15e979beed382f8cb25b2a048f8b01f4004
rishab2113 <rishab_nayak@yahoo.com>
2018-06-22 21:04:30 -0400

    gha

--- x.py
+++ x.py
@@ -9,8 +9,7 @@ addr = 0x40           # I2C address of the PWM chip.
 bus.write_byte_data(addr, 0, 0x20)     # enable the chip
 bus.write_byte_data(addr, 0xfe, 0x1e)  # configure the chip for multi-byte write
 
-bus.write_word_data(addr, 0x06, 0)     # chl 0 start time = 0us
-bus.write_word_data(addr, 0x08, 1250)  # chl 0 end time = 1.5ms
+bus.write_word_data(addr, 0x06, 0)     # chl 0 start time = 0us  # chl 0 end time = 1.5ms
 
 ## The 1.5ms end time is equal to 1.2us per count. This represents the neutral
 ##  position of the servo, midway between both extremes. Each degree of
@@ -22,7 +21,5 @@ bus.write_word_data(addr, 0x08, 1250)  # chl 0 end time = 1.5ms
 ##  from now on we only need perform the second write, as the first register we
 ##  wrote to can remain at zero.
 
-time.sleep(1)   # pause at neutral for two seconds
-bus.write_word_data(addr, 0x08, 836)  # chl 0 end time = 1.0ms
 time.sleep(1)
 bus.write_word_data(addr, 0x08, 1664)  # chl 0 end time = 2.0ms
--- y.py
+++ y.py
@@ -10,7 +10,7 @@ bus.write_byte_data(addr, 0, 0x20)     # enable the chip
 bus.write_byte_data(addr, 0xfe, 0x1e)  # configure the chip for multi-byte write
 
 bus.write_word_data(addr, 0x0A, 0)     # chl 0 start time = 0us
-bus.write_word_data(addr, 0x0C, 1250)  # chl 0 end time = 1.5ms
+  # chl 0 end time = 1.5ms
 
 ## The 1.5ms end time is equal to 1.2us per count. This represents the neutral
 ##  position of the servo, midway between both extremes. Each degree of
@@ -22,7 +22,5 @@ bus.write_word_data(addr, 0x0C, 1250)  # chl 0 end time = 1.5ms
 ##  from now on we only need perform the second write, as the first register we
 ##  wrote to can remain at zero.
 
-time.sleep(1)   # pause at neutral for two seconds
-bus.write_word_data(addr, 0x0C, 836)  # chl 0 end time = 1.0ms
 time.sleep(1)
 bus.write_word_data(addr, 0x0C, 1664)  # chl 0 end time = 2.0ms
