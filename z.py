6eb7271b45446bb8800f921c8a9a07710a015f30
Rishab Nayak <31130737+rishab2113@users.noreply.github.com>
2018-06-23 22:17:18 -0400

    nxt

--- z.py
+++ z.py
@@ -9,18 +9,7 @@ addr = 0x40           # I2C address of the PWM chip.
 bus.write_byte_data(addr, 0, 0x20)     # enable the chip
 bus.write_byte_data(addr, 0xfe, 0x1e)  # configure the chip for multi-byte write
 
-bus.write_word_data(addr, 0x0E, 0)     # chl 0 start time = 0us  # chl 0 end time = 1.5ms
-
-## The 1.5ms end time is equal to 1.2us per count. This represents the neutral
-##  position of the servo, midway between both extremes. Each degree of
-##  deviation from neutral requires that number (1250) to be changed by 4.6.
-##  Thus, the 90 degree offset from neutral requires that 414 counts (90*4.6)
-##  be added or subtracted from 1250.
-
-## Just for fun, we'll swing the servo to one side and then the other. Note that
-##  from now on we only need perform the second write, as the first register we
-##  wrote to can remain at zero.
-time.sleep(1)   # pause at neutral for one seconds
-bus.write_word_data(addr, 0x10, 836)  # chl 0 end time = 1.0ms
-time.sleep(1)
-bus.write_word_data(addr, 0x10, 1664)  # chl 0 end time = 2.0ms
+bus.write_word_data(addr, 0x0E, 0)
+bus.write_word_data(addr, 0x10, 1664)
+time.sleep(0.7)
+bus.write_word_data(addr, 0x10, 0)
