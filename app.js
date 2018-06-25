f191aa325dc46c584aea55218434024eb958e166
AtlasUnion <zruan@bu.edu>
2018-06-25 16:59:27 -0400

    revert

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ app.get('/', (req, res) => {
   res.render('homepage.html');
 });
 function runservo1() {
-  global.proc1 = exec.spawn("python x.py");
+  exec.exec("python x.py");
   return;
 }
 function runservo2() {
-  global.proc2 = exec.spawn("python y.py");
+  exec.exec("python y.py");
   return;
 }
 function runservo3() {
-  global.proc3 = exec.spawn("python z.py");
+  exec.exec("python z.py");
   return;
 }
 io.on('connection',function(socket){
@@ -27,9 +27,6 @@ io.on('connection',function(socket){
   });
   socket.on('buttonpress',function(){
     clearTimeout(global.timer)
-    setTimeout(_ => "proc1" in global ? global.proc1.kill() : void null, 0)
-    setTimeout(_ => "proc2" in global ? global.proc2.kill() : void null, 0)
-    setTimeout(_ => "proc3" in global ? global.proc3.kill() : void null, 0)
     for (var k = 0; k < global.count1; k++) {
       setTimeout(_ => runservo1(), 1000*k);
     }
@@ -58,9 +55,9 @@ function handledata() {
       for (var j in data[i]){
         if (j == time){
           io.emit('alert',"Time to take your Pills!");
-          global.count1 = data[i][j][0];
-          global.count2 = data[i][j][1];
-          global.count3 = data[i][j][2];
+          global.count1 = data[i][j]["0"];
+          global.count2 = data[i][j]["1"];
+          global.count3 = data[i][j]["2"];
           global.timer = setTimeout(function(){
             io.emit('alert',"Second Alert!");
             // additional actions here
