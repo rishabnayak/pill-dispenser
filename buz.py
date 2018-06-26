ba2849bbc173129c83867a5591785c73d5f2a802
Ryan Ruan <zruan@bu.edu>
2018-06-25 23:25:09 -0400

    added new version of buz.py

---
+++ buz.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+
+Buzzer = 11
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(Buzzer, GPIO.OUT)
+	global Buzz
+	Buzz = GPIO.PWM(Buzzer, 440)
+	Buzz.start(50)
+
+def destory():
+	Buzz.stop()
+	GPIO.output(Buzzer, 1)
+	GPIO.cleanup()
+
+if __name__=='__main__':
+	setup()
+	while True:
+		time.sleep(1)
+		Buzz.ChangeDutyCycle(0)
+		time.sleep(0.5)
+		Buzz.ChangeDutyCycle(50)
+
+	destory()
